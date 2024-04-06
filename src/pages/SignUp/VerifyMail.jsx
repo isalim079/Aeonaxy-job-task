2c65e11a96d09a1a29e86c907a100dc90c6ddda3
isalim079 <salimreza1025@gmail.com>
2024-04-06 10:16:10 +0600

    verify email step responsive done

--- src/pages/SignUp/VerifyMail.jsx
+++ src/pages/SignUp/VerifyMail.jsx
@@ -6,25 +6,27 @@ import Footer from "../Footer/Footer";
 const VerifyMail = () => {
     return (
         <div>
+            <div className="hidden md:block">
             <Navbar />
-            <div className="text-center space-y-7 my-16">
-                <h2 className="text-4xl font-semibold">
+            </div>
+            <div className="text-center space-y-5 my-10 px-3 md:px-0 md:space-y-7 md:my-16">
+                <h2 className="md:text-4xl text-2xl font-semibold">
                     Please verify your email...
                 </h2>
                 <div className="flex justify-center items-center">
-                    <img className="w-32" src={message} alt="" />
+                    <img className="md:w-32 w-20" src={message} alt="" />
                 </div>
-                <p className="text-gray-500">
+                <p className="text-gray-500 text-sm md:text-base">
                     Please verify your email address. We've sent confirmation
                     email to:
                 </p>
                 <p className="font-bold">account@refero.design</p>
-                <p className="text-gray-500">
+                <p className="text-gray-500 text-sm md:text-base">
                     Click the confirmation link in that email to begin using
                     Aeonaxy
                 </p>
                 <div className="flex justify-center items-center">
-                    <p className="text-gray-500 w-full max-w-[45%]">
+                    <p className="text-gray-500 w-full md:max-w-[45%] text-sm md:text-base">
                         Didn't receive the email? Check your spam folder, it may
                         have been caught by a filter. If you still don't see it.
                         You can{" "}
@@ -33,14 +35,16 @@ const VerifyMail = () => {
                         </span>
                     </p>
                 </div>
-                <p className="text-gray-500">
+                <p className="text-gray-500 text-sm md:text-base">
                     Wrong email address?{" "}
                     <span className="text-pink-600 font-semibold cursor-pointer">
                         Change it.
                     </span>
                 </p>
             </div>
+            <div className="hidden md:block">
             <Footer />
+            </div>
         </div>
     );
 };
